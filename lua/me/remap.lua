21af0eb42ff42462d88dfb7334ca4645e20d749e
Zane Helton <zane99@me.com>
2026-02-14 03:17:11 -0500

    Remove C-(w/j/k/l) for window jumping

--- lua/me/remap.lua
+++ lua/me/remap.lua
@@ -24,10 +24,6 @@ vim.keymap.set("n", "<leader>ws", "<C-w>s", { desc = "Split below" })
 vim.keymap.set("n", "<leader>wv", "<C-w>v", { desc = "Split right" })
 vim.keymap.set("n", "<leader>wd", "<C-w>c", { desc = "Close window" })
 vim.keymap.set("n", "<leader>w=", "<C-w>=", { desc = "Balance windows" })
-vim.keymap.set("n", "<leader>wh", "<C-w>h", { desc = "Window left" })
-vim.keymap.set("n", "<leader>wj", "<C-w>j", { desc = "Window down" })
-vim.keymap.set("n", "<leader>wk", "<C-w>k", { desc = "Window up" })
-vim.keymap.set("n", "<leader>wl", "<C-w>l", { desc = "Window right" })
 
 -- File management
 vim.keymap.set("n", "<leader>f", "<nop>", { desc = "file" })
