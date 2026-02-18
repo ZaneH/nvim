b8e3a8679f185013db6f5f0da5758fe538002619
Zane Helton <zane99@me.com>
2026-02-18 15:02:24 -0500

    Improve file navigation

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -1,4 +1,23 @@
-require("oil").setup()
+require("oil").setup({
+	keymaps = {
+		["g?"] = { "actions.show_help", mode = "n" },
+		["<CR>"] = "actions.select",
+		["<C-v>"] = { "actions.select", opts = { vertical = true } },
+		["<C-s>"] = { "actions.select", opts = { horizontal = true } },
+		["<C-t>"] = { "actions.select", opts = { tab = true } },
+		["<C-p>"] = "actions.preview",
+		["<C-c>"] = { "actions.close", mode = "n" },
+		["<C-l>"] = "actions.refresh",
+		["-"] = { "actions.parent", mode = "n" },
+		["_"] = { "actions.open_cwd", mode = "n" },
+		["`"] = { "actions.cd", mode = "n" },
+		["g~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+		["gs"] = { "actions.change_sort", mode = "n" },
+		["gx"] = "actions.open_external",
+		["g."] = { "actions.toggle_hidden", mode = "n" },
+		["g\\"] = { "actions.toggle_trash", mode = "n" },
+	},
+})
 
 vim.keymap.set("n", "<leader>pv", function()
 	vim.cmd([[Oil]])
